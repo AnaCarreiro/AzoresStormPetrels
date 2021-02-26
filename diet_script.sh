ca3516b6d3ab0c3e7714eb8078821362dd433d21
AnaCarreiro <32959532+AnaCarreiro@users.noreply.github.com>
2021-02-26 15:41:11 +0000

    Update diet_script.sh

--- diet_script.sh
+++ diet_script.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/bash
 
-####################################################################################################################################################################################
+
+####################################################################################################################################################################################
 
 ###Start of commands###
 
-####################################################################################################################################################################################
+####################################################################################################################################################################################
+
 ##Unzip fastq.gz files##
 $gunzip *.gz
 
@@ -15,12 +17,13 @@ $usearch -fastx_info RM-amplicons050819_S1_L001_R2_001.fastq
 ##Merge pairs##
 $usearch -fastq_mergepairs RM-amplicons050819_S1_L001_R1_001.fastq -fasqout ../out/pairs.fq
 
-##Demultiplex by Forward##
+
+##Demultiplex by Forward##
 
 $~/.local/bin/cutadapt -g file:Forward_Tags.fas  -o ../out/Azores{name}.fasta pairs.fastq -e 0.1 --length-tag "length="
 
 
-###Demultiplex by Reverse###
+###Demultiplex by Reverse###
 
 $~/.local/bin/cutadapt -a file:../fq/Reverse_Tags.fas -o AzoresF1{name}.fasta AzoresF1.fasta -e 0.1 -x F1{name}. --minimum-length 260 --maximum-length 280
 $~/.local/bin/cutadapt -a file:../fq/Reverse_Tags.fas -o AzoresF2{name}.fasta AzoresF2.fasta -e 0.1 -x F2{name}. --minimum-length 260 --maximum-length 280
@@ -39,13 +42,15 @@ cat AzoresF5R*.fasta > mergedF5.fasta
 cat mergedF*.fasta > merged.fasta
 
 
-###Uniques###
+###
+Uniques###
 $usearch -fastx_uniques ../merged.fasta -fastaout uniques.fasta -relabel Uniq -sizeout
 
 ##OTU's##
 $usearch -cluster_otus uniques.fasta -minsize 2 -otus otus.fasta -relabel Otu
 $usearch -usearch_global ../merged.fasta -db otus.fasta -strand plus -id 0.97 -otutabout otutable.txt
 
-
+
+
 
 
